92ea03be3da286fc3829ba48305239ea7ff7a6b7
9032lu <511815816@qq.com>
2019-07-30 22:43:52 +0800

    Initial commit

---
+++ pages/library/library.js
@@ -0,0 +1,77 @@
+// pages/library/library.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      datalist:[
+        {imgName:"dx.png",title:"图书馆_1"},
+        { imgName: "dx.png", title: "图书馆_2"},
+        { imgName: "dx.png", title: "图书馆_3"},
+        { imgName: "dx.png", title: "图书馆_4"},
+        { imgName: "dx.png", title: "图书馆_5"},
+        { imgName: "dx.png", title: "图书馆_6"},
+        { imgName: "dx.png", title: "图书馆_7"},
+        { imgName: "dx.png", title: "图书馆_8"},
+        { imgName: "dx.png", title: "图书馆_9"},
+
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
